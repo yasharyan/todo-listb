554af44cd11152df8e222373877b81e41e24f2e5
yasharyan <997yashraj@gmail.com>
2020-10-08 14:44:38 +0530

    Updates

---
+++ precache-manifest.d6e7a85bcd9742d70f690d4683ccf687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81713a5b8566500614581f02974317cd",
+    "url": "/todo-listb/index.html"
+  },
+  {
+    "revision": "fe164725f0964a5d5b6c",
+    "url": "/todo-listb/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5ed299f8c17d7b7648a1",
+    "url": "/todo-listb/static/css/main.aecd88b9.chunk.css"
+  },
+  {
+    "revision": "fe164725f0964a5d5b6c",
+    "url": "/todo-listb/static/js/2.d65a9164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-listb/static/js/2.d65a9164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed299f8c17d7b7648a1",
+    "url": "/todo-listb/static/js/main.7145acb9.chunk.js"
+  },
+  {
+    "revision": "01399f08f2e099781a58",
+    "url": "/todo-listb/static/js/runtime-main.b9ed7aee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-list/precache-manifest.87b27c3c737e56a58fcbfb683b99693f.js"
+  "/todo-listb/precache-manifest.d6e7a85bcd9742d70f690d4683ccf687.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-listb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
